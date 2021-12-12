be8dc31935af1b577d9696ffdba99cc0add94969
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2021-12-12 16:25:04 +0900

    多分input できた

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -20,19 +20,143 @@ use std::time::SystemTime;
 #[allow(dead_code)]
 const MOD: usize = 1e9 as usize + 7;
 
-#[fastout]
+const N: usize = 400;
+const M: usize = 1995;
+
+const SIDE: usize = 800;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct Coord {
+    x: isize,
+    y: isize,
+}
+
+#[allow(dead_code)]
+impl Coord {
+    fn new(p: (isize, isize)) -> Self {
+        Coord { x: p.0, y: p.1 }
+    }
+    fn from_usize_pair(p: (usize, usize)) -> Self {
+        Coord {
+            x: p.0 as isize,
+            y: p.1 as isize,
+        }
+    }
+
+    fn in_field(&self) -> bool {
+        (0 <= self.x && self.x <= SIDE as isize) && (0 <= self.y && self.y <= SIDE as isize)
+    }
+
+    // ペアへの変換
+    fn to_pair(&self) -> (isize, isize) {
+        (self.x, self.y)
+    }
+
+    // マンハッタン距離
+    fn distance(&self, that: &Self) -> isize {
+        (self.x - that.x).abs() + (self.y - that.y).abs()
+    }
+
+    fn mk_4dir(&self) -> Vec<Self> {
+        let delta = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+        delta
+            .iter()
+            .map(|&p| self.plus(&Coord::new(p)))
+            .filter(|&pos| pos.in_field())
+            .collect()
+    }
+
+    fn com_to_delta(com: char) -> Self {
+        match com {
+            'U' => Coord::new((0, -1)),
+            'D' => Coord::new((0, 1)),
+            'L' => Coord::new((-1, 0)),
+            'R' => Coord::new((1, 0)),
+            _ => unreachable!(),
+        }
+    }
+
+    // 四則演算
+    fn plus(&self, that: &Self) -> Self {
+        Coord::new((self.x + that.x, self.y + that.y))
+    }
+    fn minus(&self, that: &Self) -> Self {
+        Coord::new((self.x - that.x, self.y - that.y))
+    }
+
+    fn access_matrix<'a, T>(&'a self, mat: &'a Vec<Vec<T>>) -> &'a T {
+        &mat[self.y as usize][self.x as usize]
+    }
+
+    fn set_matrix<T>(&self, mat: &mut Vec<Vec<T>>, e: T) {
+        mat[self.y as usize][self.x as usize] = e;
+    }
+}
+
 fn main() {
     let system_time = SystemTime::now();
     let mut rng = thread_rng();
 
-    input! {
-        // n: usize,
-        // v: [isize; n],
+    // input
+    let (r, w) = (std::io::stdin(), std::io::stdout());
+    let mut sc = IO::new(r.lock(), w.lock());
+
+    let _: usize = sc.read();
+
+    let mut xy = Vec::with_capacity(N);
+    let mut uv = Vec::with_capacity(M);
+    for _ in 0..N {
+        let x: usize = sc.read();
+        let y: usize = sc.read();
+        xy.push(Coord::from_usize_pair((x, y)));
+    }
+    for _ in 0..M {
+        let u: usize = sc.read();
+        let v: usize = sc.read();
+        uv.push((u, v));
     }
 
-    let ans = 42;
+    // main loop
+    for i in 0..M {
+        let l: usize = sc.read();
 
-    println!("{}", ans);
+        // output
+        println!("{}", 1);
+    }
 
     eprintln!("{}ms", system_time.elapsed().unwrap().as_millis());
+}
+
+pub struct IO<R, W: std::io::Write>(R, std::io::BufWriter<W>);
+
+impl<R: std::io::Read, W: std::io::Write> IO<R, W> {
+    pub fn new(r: R, w: W) -> IO<R, W> {
+        IO(r, std::io::BufWriter::new(w))
+    }
+    pub fn write<S: ToString>(&mut self, s: S) {
+        use std::io::Write;
+        self.1.write(s.to_string().as_bytes()).unwrap();
+    }
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .0
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r' || b == b'\t')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r' && b != b'\t')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
 }
