8ce85d72d2ebd22316c3c48b00097925364becc6
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2021-12-12 17:08:01 +0900

    UnionFind

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -132,16 +132,79 @@ fn main() {
 
     let mut input = Input::new(xy, uv);
 
+    // main
+    let mut uf = UnionFind::new();
+
     // main loop
     for i in 0..M {
         let l: usize = sc.read();
         input.l.push(l);
 
-        // output
-        println!("{}", 1);
+        let (u, v) = input.uv[i];
+        if !uf.is_connect(u, v) {
+            println!("{}", 1);
+            uf.connect(u, v);
+        } else {
+            println!("{}", 0);
+        }
     }
 
     eprintln!("{}ms", system_time.elapsed().unwrap().as_millis());
+}
+
+struct UnionFind {
+    uni: Vec<isize>, // 根であれば *そのグループの要素数(負)* が、子であれば親の番号が入る。
+}
+
+#[allow(dead_code)]
+impl UnionFind {
+    fn new() -> Self {
+        UnionFind { uni: vec![-1; N] }
+    }
+
+    // 頂点 v の所属するグループを調べる
+    fn root(&mut self, v: usize) -> usize {
+        if self.uni[v] < 0 {
+            v
+        } else {
+            self.uni[v] = self.root(self.uni[v] as usize) as isize;
+            self.uni[v] as usize
+        }
+    }
+
+    // 頂点 a と頂点 b を繋ぐ。元々同じグループのとき　false を返す
+    fn connect(&mut self, a: usize, b: usize) -> bool {
+        let mut root_a = self.root(a) as usize;
+        let mut root_b = self.root(b) as usize;
+
+        if root_a == root_b {
+            return false;
+        }
+
+        // a 側が大きいグループになるようにスワップ
+        if self.uni[root_a] > self.uni[root_b] {
+            root_a ^= root_b;
+            root_b ^= root_a;
+            root_a ^= root_b;
+        }
+
+        // root_a と root_b を結合し、root_b の親を root_a とする
+        self.uni[root_a] += self.uni[root_b];
+        self.uni[root_b] = root_a as isize;
+
+        return true;
+    }
+
+    // 頂点 a, b が同じグループであるかを調べる
+    fn is_connect(&mut self, a: usize, b: usize) -> bool {
+        self.root(a) == self.root(b)
+    }
+
+    // 頂点 v を含むグループの頂点数を調べる
+    fn size(&mut self, v: usize) -> usize {
+        let root = self.root(v);
+        self.uni[root].abs() as usize
+    }
 }
 
 pub struct IO<R, W: std::io::Write>(R, std::io::BufWriter<W>);
