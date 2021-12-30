e0695f52ed0f6a23919823ac345a5a2a21df8de2
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2021-12-30 21:32:17 +0900

    chore: --release

--- run_test.sh
+++ run_test.sh
@@ -5,7 +5,7 @@ set -eu
 test_num="0000"
 
 cargo build --release
-cargo run --bin a < "tools/in/${test_num}.txt" > "tools/out/${test_num}.txt"
+cargo run --release --bin a < "tools/in/${test_num}.txt" > "tools/out/${test_num}.txt"
 
 cd tools
 cargo run --release --bin vis "in/${test_num}.txt" "out/${test_num}.txt"
